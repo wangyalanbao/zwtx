1180e46b535f701844693e877cefce75fbbb7835
张伟 <784031932@qq.com>
2018-03-11 16:58:00 +0800

    测试

---
+++ src/com/hxlm/health/web/controller/App/RegisterController.java
@@ -0,0 +1,16 @@
+package com.hxlm.health.web.controller.App;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2018/3/11.
+ */
+@Controller
+@RequestMapping("/register")
+public class RegisterController extends BaseController{
+
+
+
+
+}
